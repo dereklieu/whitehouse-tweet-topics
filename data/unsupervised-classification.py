212052069f207f5dcdaf7755aadc05c17a0133f0
Derek Lieu <dl@dereklieu.com>
2016-12-18 16:50:24 -0500

    Write classified json to file

--- data/unsupervised-classification.py
+++ data/unsupervised-classification.py
@@ -1,5 +1,4 @@
-import logging
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+import re
 from pprint import pprint
 from nltk.corpus import stopwords
 from nltk.tokenize import wordpunct_tokenize
@@ -10,9 +9,11 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
 from sklearn import metrics
 
-import numpy as np
-
+#import matplotlib.pyplot as plt
+#import numpy as np
 import json
+
+K = 10
 
 # read json as native structure
 def read_json(filename):
@@ -28,37 +29,61 @@ def write_json(filename, data):
 
 
 def validate_word(word):
-    if word[0] == '@' or word == 'amp;' or word[0:2] == '.@' or word[0:4] == 'http':
+    if word[0] == '@' or word == 'amp;' or word[0:2] == '.@':
         return False
     else:
         return True
 
-raw = read_json('./output/flotus.json')
+raw = read_json('./output/whitehouse.json')
 
 # only get the text for now (later I might need the tweet ids)
 documents = [x['text'] for x in raw]
 
 stoplist = set(stopwords.words('english'))
 corpus = []
-for sentence in documents:
+for tweet in documents:
+
+    tweet = tweet.lower()
+
+    # convert www.* or https?://* to URL
+    tweet = re.sub('((www\.[^\s]+)|(https?://[^\s]+))','URL',tweet)
+
+    # replace #word with word
+    tweet = re.sub(r'#([^\s]+)', r'\1', tweet)
+
+    # trim
+    tweet = tweet.strip('\'"')
 
     # filter out common twitter words like '.@', 'links, etc.
-    sentence = ' '.join([word for word in sentence.lower().split() if validate_word(word)])
+    tweet = ' '.join([word for word in tweet.split() if validate_word(word)])
 
     # tokenize and remove stoplists, punctuation, random numbers.
-    tokenized = [word for word in wordpunct_tokenize(sentence) if word not in stoplist and len(word) > 1]
+    tokenized = [word for word in wordpunct_tokenize(tweet) if word not in stoplist and len(word) > 1]
 
     # stem, join, and append to list.
     corpus.append(' '.join([stemmer.stem(word) for word in tokenized]).encode('utf8'))
 
+# vectorize using tfidf
 vectorizer = TfidfVectorizer()
 X = vectorizer.fit_transform(corpus)
 print X.shape
 
-km = KMeans(n_clusters=10, max_iter=500)
+km = KMeans(n_clusters=K, max_iter=500)
 km.fit(X)
 
 clustered_labels = km.labels_
-for idx, sentence in enumerate(documents):
-    if clustered_labels[idx] == 0:
-        print sentence
+aggregate = [[] for i in range(0, K)]
+for idx, label in enumerate(clustered_labels):
+    raw_data = raw[idx]
+
+    # only need the first part, ie 2016-11-12
+    tweeted_at = raw_data['timestamp'].split(' ')[0].split('-')
+    result = {}
+    result['text'] = raw_data['text'].encode('utf-8')
+    result['id'] = raw_data['tweet_id']
+    result['year'] = tweeted_at[0]
+    result['month'] = tweeted_at[1]
+    result['day'] = tweeted_at[2]
+    aggregate[label].append(result)
+
+write_json('output/categorized.json', aggregate)
