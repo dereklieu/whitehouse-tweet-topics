7b912660abd0af0257854ccd955bf786deda32c8
Derek Lieu <dl@dereklieu.com>
2017-01-04 18:41:35 -0500

    Tweak categorization

--- tool/unsupervised-classification.py
+++ tool/unsupervised-classification.py
@@ -13,7 +13,7 @@ from sklearn import metrics
 #import numpy as np
 import json
 
-K = 12
+K = 10
 
 # read json as native structure
 def read_json(filename):
@@ -29,7 +29,7 @@ def write_json(filename, data):
 
 
 def validate_word(word):
-    if word[0] == '@' or word == 'amp;' or word[0:2] == '.@' or word == 'rt':
+    if word == 'amp;' or word == 'rt' or word == '...':
         return False
     else:
         return True
@@ -51,6 +51,15 @@ for tweet in documents:
     # replace #word with word
     tweet = re.sub(r'#([^\s]+)', r'\1', tweet)
 
+    # remove em dashes
+    tweet = re.sub(u'\2014', '', tweet)
+
+    # common hashtag
+    tweet = re.sub('hicp', 'healthcare', tweet)
+
+    # normalize common shorthand
+    tweet = re.sub('vets', 'veteran', tweet)
+
     # trim
     tweet = tweet.strip('\'"')
 
@@ -58,17 +67,17 @@ for tweet in documents:
     tweet = ' '.join([word for word in tweet.split() if validate_word(word)])
 
     # tokenize and remove stoplists, punctuation, random numbers.
-    tokenized = [word for word in wordpunct_tokenize(tweet) if word not in stoplist and len(word) > 1]
+    tokenized = [word for word in wordpunct_tokenize(tweet) if word not in stoplist and len(word) > 2]
 
     # stem, join, and append to list.
     corpus.append(' '.join([stemmer.stem(word) for word in tokenized]).encode('utf8', 'ignore'))
 
 # vectorize using tfidf
-vectorizer = TfidfVectorizer()
+vectorizer = TfidfVectorizer(max_df=0.5, min_df=2, use_idf=1)
 X = vectorizer.fit_transform(corpus)
 print X.shape
 
-km = KMeans(n_clusters=K, max_iter=200, n_init=20)
+km = KMeans(init='k-means++', n_init=1, n_clusters=K, max_iter=10000, verbose=0)
 km.fit(X)
 
 clustered_labels = km.labels_
@@ -79,7 +88,7 @@ for idx, label in enumerate(clustered_labels):
     # only need the first part, ie 2016-11-12
     tweeted_at = raw_data['timestamp'].split(' ')[0].split('-')
     result = {}
-    result['text'] = raw_data['text']
+    result['text'] = raw_data['text'].encode('utf-8')
     result['corpus'] = corpus[idx]
     result['id'] = raw_data['tweet_id']
     result['year'] = tweeted_at[0]
